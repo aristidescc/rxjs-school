6fcdd4f903ad53db4f1cc77739a40af07a6e10fa
Ari Castillo <acastillo@cswsolutions.com>
2024-04-09 10:27:09 -0400

    Cambios para leccion 2

--- src/create.js
+++ src/create.js
@@ -1,15 +1,17 @@
-import { displayLog } from './utils';
-import { Observable } from 'rxjs';
+import { displayLog } from "./utils";
+import { Observable } from "rxjs";
 
 export default () => {
-    /** start coding */
-    const hello = Observable.create(function(observer) {
-        observer.next('Hello');
-        setTimeout(()=>{
-            observer.next('World');
-        }, 2000);
-    });
-    
-    const subscribe = hello.subscribe(evt => displayLog(evt));
-    /** end coding */
-}
+  /** start coding */
+  const hello = Observable.create(function (observer) {
+    observer.next("Hello");
+    setTimeout(() => {
+      observer.next("World");
+    }, 2000);
+
+    observer.complete();
+  });
+
+  const subscribe = hello.subscribe((evt) => displayLog(evt));
+  /** end coding */
+};
