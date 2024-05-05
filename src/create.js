cd7f4a3991e8798d8798b3d30a3a87446a7c4d3c
Ari Castillo <acastillo@cswsolutions.com>
2024-05-04 20:36:48 -0400

    Cambios en leccion 2 - 2

--- src/create.js
+++ src/create.js
@@ -7,11 +7,16 @@ export default () => {
     observer.next("Hello");
     setTimeout(() => {
       observer.next("World");
+      observer.complete();
     }, 2000);
-
-    observer.complete();
   });
 
-  const subscribe = hello.subscribe((evt) => displayLog(evt));
+  const observer = {
+    next: (evt) => displayLog(evt),
+    error: (err) => console.err("[ERR] - ", err),
+    complete: () => displayLog("[DONE]"),
+  };
+
+  const subscribe = hello.subscribe(observer);
   /** end coding */
 };
